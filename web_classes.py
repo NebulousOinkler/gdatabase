7d5fe0bf80f1d5f5dcc92f221d6cbfb553f5d1d4
NebulousOinkler <34411516+NebulousOinkler@users.noreply.github.com>
2018-04-21 16:45:04 -0700

    updated

--- web_classes.py
+++ web_classes.py
@@ -1,28 +1,29 @@
 import sqlite3
-
 class web_search:
 	def __init__(self):
 		self.conn = sqlite3.connect('gdatabase.db')
-		self.c = conn.cursor()
+		self.c = self.conn.cursor()
 
-	def search(qtype, arg):
+	def search(self,qtype, arg):
 		if qtype == 'deg_seq':
-			raw_entries = self.c.execute("""SELECT Refs.bib, GraphRefDetails.contrib, Graphs.vert, Graphs.edges, Graphs.name FROM GraphRefDetails 
+			like = "'" + arg + "%'"
+			raw_entries = self.c.execute("""SELECT Refs.bib, GraphRefDetails.contrib, Graphs.vert, Graphs.edges, Graphs.name, Graphs.deg_seq FROM GraphRefDetails 
 			INNER JOIN Graphs ON GraphRefDetails.gid = Graphs.gid 
 			INNER JOIN Refs ON GraphRefDetails.rid = Refs.rid 
-			WHERE deg_seq LIKE (?)%""",(arg,)).fetchall()
-
+			WHERE deg_seq LIKE"""+ like).fetchall()
+			
 			result = self.structure(raw_entries)
 
-			return result
+		return result
 
 	def structure(self, raw_entries):
-		return web_entry(raw_entries)
+		r = [web_entry(e) for e in raw_entries]
+		return r
 
 class web_entry:
 	def __init__(self, raw_entries):
 		r = raw_entries
-		self.bib, self.contrib, self.vert, self.edges, self.name = raw_entries
+		self.bib, self.contrib, self.vert, self.edges, self.name, self.deg_seq = raw_entries
 
 	def display(self):
 		pass
